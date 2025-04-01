451d5b94b336c877cf9121e03cf4bda0030cd546
Vanshikathakur04 <thakurvanshika876@gmail.com>
2025-04-01 18:30:45 +0530

    Navbar created and tailwind integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
+import Navbar from './components/Navbar';
+import Manager from './components/Manager';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      PassApp
+      <Navbar />
+      <Manager />
     </>
   )
 }
