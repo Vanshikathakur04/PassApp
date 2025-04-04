b6b338696f74f81cbb45fb1a022e91d6c13df3f6
Vanshikathakur04 <thakurvanshika876@gmail.com>
2025-04-04 17:59:32 +0530

    refactor: Removed navigation links and simplified Navbar layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,13 +11,7 @@ const Navbar = () => {
         <span>Pass</span><span className="text-green-500">OP/&gt;</span>
         
       </div>
-      {/* <ul>
-         <li className='flex gap-4'>
-             <a className='hover:font-bold' href="/">Home</a>
-             <a className='hover:font-bold' href="#">About</a>
-             <a className='hover:font-bold' href="#">Contact</a>
-          </li>
-      </ul> */}
+      
       <button className='text-white bg-green-700 my-5 rounded-full flex justify-between items-center ring-white ring-1'>
           <img className='invert w-10 p-1' src="public/icons-github.svg" alt="github logo" />
           <span className="font-bold px-2">Github</span>
